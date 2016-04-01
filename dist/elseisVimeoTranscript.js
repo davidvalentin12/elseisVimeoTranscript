8d355cda6192beaa92daef4cda21fdcb132360a6
MOHRING <David.Valentin@t-systems.com>
2016-04-01 17:54:11 +0200

    elseisVerFicha button id

--- dist/elseisVimeoTranscript.js
+++ dist/elseisVimeoTranscript.js
@@ -10,6 +10,47 @@
 
 (function() {
   'use strict';
+  angular.module('elseisVimeoTranscript')
+      .directive('elseisVimeoTranscript', function() {
+        return {
+          restrict: 'A',
+          controller: 'elseisVimeoTranscriptCtrl',
+          controllerAs: 'elseisVimeoTranscriptCtrl',
+          bindToController: true,
+          templateUrl: 'src/templates/elseisVimeoTranscript.tpl.html',
+          scope: {
+            transcriptConfig: '='
+          },
+          link: function(scope, element, attributes) {
+          }
+        };
+      });
+
+}());
+
+(function () {
+  'use strict';
+
+
+  angular.module('elseisVimeoTranscript')
+      .filter('sanitize', ['$sce', function ($sce) {
+        return function (htmlCode) {
+          return $sce.trustAsHtml(htmlCode);
+        }
+      }])
+      .filter('trusted', ['$sce', function ($sce) {
+        return function(url) {
+          return $sce.trustAsResourceUrl(url);
+        };
+      }])
+      .filter('milisecondsToDateTime', [function() {
+        return function(miliseconds) {
+          return new Date(1970, 0, 1).setSeconds(miliseconds/1000);
+        };
+      }]);
+}());
+(function() {
+  'use strict';
 
   angular.module('elseisVimeoTranscript')
       .controller('elseisVimeoTranscriptCtrl', ["$scope", "$http", "$rootScope", "$sce", function($scope, $http, $rootScope, $sce) {
@@ -75,46 +116,5 @@
         });
 
 
-      }]);
-}());
-(function() {
-  'use strict';
-  angular.module('elseisVimeoTranscript')
-      .directive('elseisVimeoTranscript', function() {
-        return {
-          restrict: 'A',
-          controller: 'elseisVimeoTranscriptCtrl',
-          controllerAs: 'elseisVimeoTranscriptCtrl',
-          bindToController: true,
-          templateUrl: 'src/templates/elseisVimeoTranscript.tpl.html',
-          scope: {
-            transcriptConfig: '='
-          },
-          link: function(scope, element, attributes) {
-          }
-        };
-      });
-
-}());
-
-(function () {
-  'use strict';
-
-
-  angular.module('elseisVimeoTranscript')
-      .filter('sanitize', ['$sce', function ($sce) {
-        return function (htmlCode) {
-          return $sce.trustAsHtml(htmlCode);
-        }
-      }])
-      .filter('trusted', ['$sce', function ($sce) {
-        return function(url) {
-          return $sce.trustAsResourceUrl(url);
-        };
-      }])
-      .filter('milisecondsToDateTime', [function() {
-        return function(miliseconds) {
-          return new Date(1970, 0, 1).setSeconds(miliseconds/1000);
-        };
       }]);
 }());
